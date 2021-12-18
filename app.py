bc7d95fa99c88d281831a5372d17b4f43388d8e9
merveealpay <merveealpay@gmail.com>
2021-12-18 23:14:28 +0300

    dockerfile,dockercompose,redis

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_restful import Resource, Api
+from flask_caching import Cache
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+cache = Cache(app)  # Initialize Cache
 api = Api(app)
 
+
 class ExchangeRates(Resource):
+    @cache.cached(timeout=30, query_string=True)
     def get(self):
         API_URL = "https://openexchangerates.org/api/latest.json?app_id=1c826c16fd724747a9e0b01e9c35c103%27"
         r = requests.get(f"{API_URL}")
-        data = jsonify(r.json())
-        return data
+        all_rates = r.json()["rates"]
+        try_eur_rates = {"base": "TRY", "USD": all_rates["TRY"]}
+        return try_eur_rates
 
 
 api.add_resource(ExchangeRates, '/rates')

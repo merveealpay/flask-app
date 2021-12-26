52cbdffa711464074bc22be406a64b4f046db1d7
merveealpay <merveealpay@gmail.com>
2021-12-26 17:27:09 +0300

    add model

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 import requests
 from flask_restful import Resource, Api
 from flask_caching import Cache
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 cache = Cache(app)
 api = Api(app)
 
